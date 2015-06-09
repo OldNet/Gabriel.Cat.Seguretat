71ab5f018540529c0b686cbd050751593542acb9
tetradog <tetradog@gmail.com>
2015-06-09 16:22:05 +0200

    subir datos

---
+++ XifraBitmap.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using Gabriel.Cat;
+namespace Gabriel.Cat.Seguretat
+{
+    public enum XifratImg
+    {
+        Normal
+    }
+  public static  class XifraBitmap
+    {
+        public static Bitmap Xifra(this Bitmap img, XifratImg xifrat, NivellXifrat nivell, string text)
+        {
+            return img.Xifra(xifrat, nivell, Serializar.GetBytes(text));
+        }
+        public static Bitmap Xifra(this Bitmap img, XifratImg xifrat, NivellXifrat nivell, byte[] dades)
+        {
+            Bitmap imgXifrada = img.Clone() as Bitmap;
+            switch (xifrat)
+            {
+                //le pongo el texto
+                case XifratImg.Normal:INormalXifrat(img,nivell, dades);break;
+            }
+            return imgXifrada;
+        }
+
+        public static byte[] Desxifra(this Bitmap img, XifratImg xifrat, NivellXifrat nivell)
+        {
+            byte[] dades=new byte[0];
+            switch (xifrat)
+            {
+                //le quito el texto
+                case XifratImg.Normal:dades = INormalDesXifrat(img,nivell);break;
+            }
+            return dades;
+        }
+
+        private static void INormalXifrat(Bitmap img, NivellXifrat nivell, byte[] dades)
+        {
+            //pone los bytes en la imagen
+        }
+        private static byte[] INormalDesXifrat(Bitmap img, NivellXifrat nivell)
+        {
+            Llista<byte> dadesTrobades = new Llista<byte>();
+            //trec les dades
+
+            return dadesTrobades.ToTaula();
+        }
+
+    }
+}
---
+++ XifraString.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Text;
+//lo uso extendiendo string
+namespace Gabriel.Cat.Seguretat
+{
+    public enum XifratText
+    {
+        TextDisimulat
+    }
+    public enum NivellXifrat
+    {
+        MoltBaix,
+        Baix,
+        Normal,
+        Alt,
+        MoltAlt
+    }
+    public static class XifraString
+    {
+
+        public static string Xifra(this string text, XifratText xifrat, NivellXifrat nivell, string password)
+        {
+            string textXifrat = null;
+            switch (xifrat)
+            {
+                case XifratText.TextDisimulat:textXifrat = ITextDisimulatXifra(text, nivell, password);break;
+            }
+            return textXifrat;
+        }
+        public static string Desxifra(this string text, XifratText xifrat, NivellXifrat nivell, string password)
+        {
+            string textXifrat = null;
+            switch (xifrat)
+            {
+                case XifratText.TextDisimulat: textXifrat = ITextDisimulatDesxifra(text, nivell, password); break;
+            }
+            return textXifrat;
+        }
+        #region TextDisimulat
+        private static string ITextDisimulatXifra(string text, NivellXifrat nivell, string password)
+        {
+            //usa la password caracter a caracter para saber la posicion donde va el texto real...lo demas es pura basura
+            StringBuilder textXifrat = new StringBuilder();
+            if (text != "" && password != "")
+            {
+                Random llavor = new Random();
+                int posicionPassword = 0;
+                for (int i = 0; i < text.Length; i++)
+                {
+
+                    for (int j = 0, finalBasura = (int)password[posicionPassword] * (int)nivell + 1; j < finalBasura; j++)//pongo los caracteres basura
+                        textXifrat.Append((char)(llavor.Next(127) + 32));
+                    textXifrat.Append(text[i]);//pongo el caracter a disimular
+                    posicionPassword++;
+                    if (posicionPassword == password.Length)
+                        posicionPassword = 0;
+                }
+            }
+            else return text;
+            return textXifrat.ToString();
+        }
+        private static string ITextDisimulatDesxifra(string text, NivellXifrat nivell, string password)
+        {
+  
+                //usa la password caracter a caracter para saber la posicion donde va el texto real...lo demas es pura basura
+                StringBuilder textDesxifrat = new StringBuilder();
+            if (text != "" && password != "")
+            {
+                int posicion = 0;
+                int posicionPassword = 0;
+                //lo repito por el primer caso
+                posicion += ((int)password[posicionPassword] * (int)nivell + 1);//me salto la basura
+                posicionPassword++;
+                if (posicionPassword == password.Length)
+                    posicionPassword = 0;
+                textDesxifrat.Append(text[posicion]);
+                while (posicion < text.Length)
+                {
+                    posicion += ((int)password[posicionPassword] * (int)nivell + 1) + 1;//me salto la basura
+                    posicionPassword++;
+                    if (posicionPassword == password.Length)
+                        posicionPassword = 0;
+                    if (text.Length > posicion)
+                        textDesxifrat.Append(text[posicion]);
+                }
+            }
+            else return text;
+            return textDesxifrat.ToString();
+        }
+        #endregion
+    }
+}
