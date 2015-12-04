2526988b0adbc55629a84af66934b4a65bc32b1f
tetradog <tetradog@gmail.com>
2015-12-04 22:05:45 +0100

    arreglado problemas subida...falta acabar Serializar para poder compilar

--- XifraBitmap.cs
+++ XifraBitmap.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using Gabriel.Cat;
+using Gabriel.Cat.Extension;
 using System.Collections;
 
 namespace Gabriel.Cat.Seguretat
