6d67e4a07dad32d4b625a545603bfd71eaf22607
tetradog <tetradog@gmail.com>
2016-02-02 01:35:51 +0100

    desarrollando Xifra bitmap

--- XifraBitmap.cs
+++ XifraBitmap.cs
@@ -9,163 +9,150 @@ using Gabriel.Cat.Extension;
 
 namespace Gabriel.Cat.Seguretat
 {
-    public enum XifratImg
-    {
-        Normal
-    }
-    public struct XifratImgNivell
-    {
-        XifratImg xifrat;
-        NivellXifrat nivell;
+	public enum XifratImg
+	{
+		Normal
+	}
+	public struct XifratImgNivell
+	{
+		XifratImg xifrat;
+		NivellXifrat nivell;
 
-        public XifratImgNivell(XifratImg xifrat, NivellXifrat nivell)
-        {
-            this.xifrat = xifrat;
-            this.nivell = nivell;
-        }
+		public XifratImgNivell(XifratImg xifrat, NivellXifrat nivell)
+		{
+			this.xifrat = xifrat;
+			this.nivell = nivell;
+		}
 
-        public XifratImg Xifrat
-        {
-            get
-            {
-                return xifrat;
-            }
+		public XifratImg Xifrat {
+			get {
+				return xifrat;
+			}
 
-        }
+		}
 
-        public NivellXifrat Nivell
-        {
-            get
-            {
-                return nivell;
-            }
-        }
-    }
-    public static class XifraBitmap
-    {
-        const int BITSBYTE = 8;
+		public NivellXifrat Nivell {
+			get {
+				return nivell;
+			}
+		}
+	}
+	public static class XifraBitmap
+	{
+		const int BITSBYTE = 8;
 
-        public static Bitmap Xifra(this string text, XifratImg xifrat, NivellXifrat nivell)
-        {
+		public static Bitmap Xifra(this string text, XifratImg xifrat, NivellXifrat nivell)
+		{
 
-            return Serializar.GetBytes(text).Xifra(xifrat, nivell);
-        }
-        public static Bitmap Xifra(this byte[] dades, XifratImg xifrat, NivellXifrat nivell)
-        {
-            //por probar, mirar que lo haga bien :)
-            //    creo una imagen random que quepa lo que quiero
-            int totalBytesImg = dades.Length* ((int)nivell + 1)*BITSBYTE;
-            int height = totalBytesImg / 2, width = totalBytesImg - height;
-            Bitmap imgRandom=new Bitmap(width,height);
-            imgRandom.RandomPixels();
-            imgRandom.Xifra(xifrat, nivell, dades);
-            return imgRandom;
-        }
-        public static void Xifra(this Bitmap img, XifratImg xifrat, NivellXifrat nivell, string text)
-        {
-            img.Xifra(xifrat, nivell, Serializar.GetBytes(text));
-        }
-        public static void Xifra(this Bitmap img, XifratImg xifrat, NivellXifrat nivell, byte[] dades)
-        {
-            switch (xifrat)
-            {
-                //le mezclo los datos
-                case XifratImg.Normal:/*dades = dades;*/break;//ejemplo
+			return Serializar.GetBytes(text).Xifra(xifrat, nivell);
+		}
+		public static Bitmap Xifra(this byte[] dades, XifratImg xifrat, NivellXifrat nivell)
+		{
+			//por probar, mirar que lo haga bien :)
+			//    creo una imagen random que quepa lo que quiero
+			int totalBytesImg = (dades.Length* BITSBYTE) * ((int)nivell + 1) ;
+			int height = totalBytesImg / 2, width = totalBytesImg - height;
+			Bitmap imgRandom = new Bitmap(width, height);
+			imgRandom.RandomPixels();
+			imgRandom.Xifra(xifrat, nivell, dades);
+			return imgRandom;
+		}
+		public static void Xifra(this Bitmap img, XifratImg xifrat, NivellXifrat nivell, string text)
+		{
+			img.Xifra(xifrat, nivell, Serializar.GetBytes(text));
+		}
+		public static void Xifra(this Bitmap img, XifratImg xifrat, NivellXifrat nivell, byte[] dades)
+		{
+			bool[] bitsAPoner;
+			int longitudImg;
+			int auxByte;
+			switch (xifrat) {
+			//le mezclo los datos
+				case XifratImg.Normal:/*dades = dades;*/
+					break;//ejemplo
 
-            }
-            IXifra(img, xifrat, nivell, dades);
+			}
+			//pone los bytes en la imagen
+			bitsAPoner = Serializar.GetBytes(dades.Length).AfegirValors(dades).ToBits();
+			
+			if (img.LengthBytes() < bitsAPoner.Length * ((int)nivell + 1))
+				throw new Exception("La imatge no pot contenir les dades amb el nivell de seguretat posat!");
+			unsafe {
+				img.TrataBytes((MetodoTratarBytePointer)((imgBytes) => {
+					longitudImg = img.LengthBytes();
+					auxByte = 0;
+				                                         	
+					for (int j = 0, i = 0, incremento = ((int)nivell + 1); i < bitsAPoner.Length; j += incremento, i++) {//pongo la longitud de los datos y luego los datos
+						if (bitsAPoner[i]) {
+							auxByte = imgBytes[j];
 
-        }
-        private static void IXifra(Bitmap img, XifratImg xifrat, NivellXifrat nivell, byte[] dades)
-        {
-            //pone los bytes en la imagen
-            bool[] bitsAPoner =Serializar.GetBytes(dades.Length).AfegirValors(dades).ToBits();
-              
-            if (img.LengthBytes() < bitsAPoner.LongLength *((int)nivell + 1)*BITSBYTE)
-                throw new Exception("La imatge no pot contenir les dades amb el nivell de seguretat posat!");
-            unsafe
-            {
-                img.TrataBytes((MetodoTratarBytePointer)((imgBytes) =>
-                {
-                    int longitudImg = img.LengthBytes();
-                    int auxByte = 0;
-              
-                    for (int j = 0, i = 0, incremento = ((int)nivell + 1); i < bitsAPoner.Length; j += incremento, i++)//pongo la longitud de los datos y luego los datos
-                {
-                        if (bitsAPoner[i])
-                        {
-                            if (imgBytes[j] % 2 != 0)
-                            {
-                                auxByte = imgBytes[j];
-                                //tiene que ser false
-                                if (auxByte % 2 == 0)
-                                {
-                                    auxByte++;
-                                    imgBytes[j] = (byte)auxByte;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            auxByte = imgBytes[j];
-                            //tiene que ser false
-                            if (auxByte % 2 != 0)
-                            {
-                                auxByte--;
-                                imgBytes[j] = (byte)auxByte;
-                            }
-                        }
-                    }
+							//tiene que ser true
+							if (auxByte % 2 == 0) {
+								auxByte++;
+								imgBytes[j] = (byte)auxByte;
+							}
+				                                         			
+						} else {
+							auxByte = imgBytes[j];
+							//tiene que ser false
+							if (auxByte % 2 != 0) {
+								auxByte--;
+								imgBytes[j] = (byte)auxByte;
+							}
+						}
+					}
 
 
-                    }
-                ));
-            }
+				}
+				));
+			}
 
-        }
-        public static byte[] Desxifra(this Bitmap img, XifratImg xifrat, NivellXifrat nivell)
-        {
-            
-            bool[] dadesBits=null;
-            byte[] dades = null;
-            bool[] bitsLongitudDades =new bool[ BITSBYTE * 4];
-            int incremento=((int)nivell+1)*BITSBYTE;
-            unsafe
-            {
-                img.TrataBytes((MetodoTratarBytePointer)((bytesImg) =>
-                {
-                    
-                    //leo la longitud
-                    for(int i=0,f=incremento*bitsLongitudDades.Length,k=0;k<bitsLongitudDades.Length;i+=incremento,k++)
-                    {
-                        bitsLongitudDades[k] = bytesImg[i] % 2 != 0;
-                    }
-                    dadesBits = new bool[Serializar.ToInt(bitsLongitudDades.ToByteArray())];
-                    //leo hasta acabar tal longitud
-                    for(int i=incremento*bitsLongitudDades.Length,f=i+(dades.Length*incremento),k=0;k<dadesBits.Length;i+=incremento,k++)
-                    {
-                        dadesBits[k] = bytesImg[i] % 2 != 0;
-                    }
-                    dades = dadesBits.ToByteArray();
-                }));
-            }
-            switch (xifrat)
-            {
-                //lo descifro
 
-                case XifratImg.Normal:/* dades = dades;*/break;//ejemplo
-            }
+		}
 
-            return dades;
-        }
+		public static byte[] Desxifra(this Bitmap img, XifratImg xifrat, NivellXifrat nivell)
+		{
+			
+			bool[] dadesBits = null;
+			byte[] dades = null;
+			bool[] bitsLongitudDades = new bool[ BITSBYTE * 4];
+			byte[] bytesLongitudDades;
+			int longitudDades;
+			int incremento = ((int)nivell + 1) * BITSBYTE;
+			unsafe {
+				img.TrataBytes((MetodoTratarBytePointer)((bytesImg) => {
+				                                         	
+					//leo la longitud
+					for (int i = 0, k = 0; k < bitsLongitudDades.Length; i += incremento,k++) {
+						bitsLongitudDades[k] = bytesImg[i] % 2 != 0;
+					}
+					bytesLongitudDades = bitsLongitudDades.ToByteArray();
+					longitudDades = Serializar.ToInt(bytesLongitudDades);
+					dadesBits = new bool[longitudDades];
+					//leo hasta acabar tal longitud
+					for (int i = incremento * bitsLongitudDades.Length, f = i + (dadesBits.Length * incremento), k = 0; k < dadesBits.Length / 8; i += incremento,k++) {
+						dadesBits[k] = bytesImg[i] % 2 != 0;
+					}
+					dades = dadesBits.ToByteArray();
+				}));
+			}
+			switch (xifrat) {
+			//lo descifro
 
-        public static int MaxBytesXifrat(this Bitmap bmp,NivellXifrat nivell)
-        {
-            return bmp.LengthBytes() / (((int)nivell + 1)* BITSBYTE);
-        }
+				case XifratImg.Normal:/* dades = dades;*/
+					break;//ejemplo
+			}
+
+			return dades;
+		}
+
+		public static int MaxBytesXifrat(this Bitmap bmp, NivellXifrat nivell)
+		{
+			return bmp.LengthBytes() / (((int)nivell + 1) * BITSBYTE);
+		}
 
 
 
 
-    }
+	}
 }
